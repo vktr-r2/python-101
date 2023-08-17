d8a82c32819d5a90313caed87a3859f79d2d3940
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-17 13:56:11 -0400

    Add opperators notes

---
+++ operators.py
@@ -0,0 +1,69 @@
+""" OPERATORS
+- enable us to perform operations on vars
+- few modify a var value, most do not
+- can be strung together, always follow order of operations left to right.  Best practice to use brackets to break up chains of operators for readability
+
+
+ARITHMETIC OPERATORS
+- Generally used for numberical calculations
+- Can be used to glue strings together (concat)
+- Produce new numerical or string result, usually leaving original numbers untouched
+
++ => addition
+- => subtraction
+* => multiplication
+/ => division
+% => modulus => returns remainder of a division
+// => floor division => preforms division and discards remainder
+** => exponentiation
+"""
+
+print("code examples here")
+
+"""
+ASSIGNMENT OPERATORS
+- Used to assign values to vars
+- Can be used in combination with arithmetic operators
+- =
+
++=
+-=
+*=
+/=
+%=
+//=
+**=
+"""
+
+print("code examples here")
+
+"""
+COMPARISON OPERATORS
+- Used to compare two values
+- Variables can be numerical, string, or booleans
+
+==
+!=
+>
+>=
+<
+<=
+
+
+LOGICAL OPERATORS
+- Used to test additional conditions
+- Typically used on booleans and control flow
+
+not
+or
+and
+
+
+OTHER OPERATORS
+
+ternary operator: if else statement
+identity operator: is, is not
+membership: in, not in
+bitwise: & , | , ^ , ~ , << , >>
+
+"""
