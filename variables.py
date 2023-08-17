25c1a112e22fa41d8fcb828e2b3fb8909173566c
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-17 12:46:31 -0400

    Add variables notes and examples

---
+++ variables.py
@@ -0,0 +1,29 @@
+
+# Python does not use NULL or NIL but instead None
+python_null = None
+
+
+""" VARIABLE TYPES
+- variable types are flexible in Python
+- you can create custom types through classes and objects
+- conversion between most types is available where it makes sense
+- value assigned to variable dictates the type automatically
+
+BOOLEANS => TRUE or FALSE
+
+FLOATS => Decimal numbers
+
+INTS => Full numbers
+
+STRINGS => Collection of text characters
+"""
+
+python_bool = True
+python_float = 1.0
+python_int = 1
+python_string = "Hello, world!"
+
+print(type(python_bool))
+print(type(python_float))
+print(type(python_int))
+print(type(python_string))
