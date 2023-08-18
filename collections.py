c854bd5c3e2926806978b1177c23726ee6f11395
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-17 23:05:56 -0400

    Add collections notes

---
+++ collections.py
@@ -0,0 +1,61 @@
+"""
+COLLECTIONS NOTES
+
+- Provide a way to store multiple values in a single variable
+- Items in collection are called elements.  Can be variables or literal valuables
+- Collections can be nested, an element can be a collection
+- Come with built in additional functionality
+
+Different Types: Lists, Tuples, Dictionaries, Ranges
+
+"""
+
+
+"""
+LISTS
+
+- Store 0 or more items at specific indexes
+- Can access elements based on their index
+- Cannot access items at indexes that don't exist, there is no array bounds auto-checks
+- Can contian multiple types of variables in Python.  This makes Python flexible, but also slower than other more strict languages
+- Mutable (modifyable)
+
+
+"""
+
+
+"""
+TUPLES
+
+- Very similar to lists
+- Access elements based on index
+- Can contain multiple types of variables
+- IMMUTABLE (cannot modify once declared)
+
+
+"""
+
+
+
+"""
+DICTIONARIES
+
+- Store key-value pairs at each index
+- Each element is a tuple of a key-value pair
+- Access values based on key, not index
+- Can also retrieve just a list of keys or just a list of values
+- MUTABLE
+
+
+"""
+
+
+"""
+RANGES
+
+- Represent lists of consecutive whole numbers but odn't have functionality of lists
+- Used for loops
+- Have to specify start and end values
+- Can also specify step value
+- Can wrap in reversed() function to count backwards
+"""
