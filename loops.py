ee89a35c7f3337f9a220f45a29b22eda428b0bda
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-18 23:22:22 -0400

    Add for loop code examples

--- loops.py
+++ loops.py
@@ -18,23 +18,24 @@ WHILE LOOPS
 
 x_position = 0
 end_position = 500
-
-
 while x_position < end_position:
   x_position += 1
   #print("loop exited")                       # INDENTATION MATTERS IN PYTHON!  => this string would print 500 times because of the indent, which includes it in the while loop
 print("loop exited")                          # Prints AFTER loop because no indentation
 
-
+x_position = 0
+end_position = 500
 enemy_position = 278
 while x_position < end_position:              # Silly example, because you will always collide with the enemy, just demonstrates that any code can go within loop
   x_position += 1
   if x_position == enemy_position:
     print("You ran into the enemy.  GAME OVER")
     break                                     # Breaks the code out of the loop
-print("You made it!")  
+print("You made it!")                         # Even though loop is broken, code will still execute because it lives outside of loop
 
 
+x_position = 0
+end_position = 500
 y_position = 1
 while x_position < end_position:              
   x_position += 1
@@ -42,8 +43,11 @@ while x_position < end_position:
     continue                                  # Ends the current iteration and moves the loop on to the next iteration                                  
   if x_position == enemy_position:            # Since y_position is always greater than 0, this if statement will never be evaluated.  Still a silly example but..
     print("You ran into the enemy.  GAME OVER")
-    break                                     #
-print("You made it!")  
+    break                                     
+print("You made it here!")  
+
+
+
 
 """
 FOR LOOPS
@@ -51,5 +55,24 @@ FOR LOOPS
 - Often referred to as for-in loops in Python
 - Run a set number of times with specific start and end points
 - Very useful if you need to visit every element in some range or list, starting at the beginning and finishing at the end
+- Need to be paired with a range or list
 """
 
+
+#SYNTAX: for variable in range/list
+
+# Using range
+for i in range(1,11):
+  print(i)
+
+
+# For loop using existing list
+inventory = ["Boots", "Cape", "Wand", "Spell Book", "Potion", "Broom stick"]
+for item in inventory:
+  print(i)
+
+
+# While loop using existing list
+while i < len(inventory):
+  print(inventory[i])
+  i += 1
