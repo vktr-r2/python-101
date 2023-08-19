8d0f9081b637c59e9402b7c8799cbde4cf7a3443
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-18 22:19:56 -0400

    Add for and while loop notes

---
+++ loops.py
@@ -0,0 +1,31 @@
+"""
+LOOPS NOTES
+
+- Loops are another form of control flow, just like conditionals
+- Provide a way to automate code to run multiple times
+- Statements are places within the loop and they are executed continuously until some break condition is met
+- Can put what ever we want in the loop body, but all code will be executed with each iteration of the loop
+"""
+
+
+
+
+"""
+WHILE LOOPS
+
+- While loops function similar to if statements but run the code continuously rather than just once
+- Code breaks out of a while loop once the test condition evaluates to false
+- MUST set a test condition, otherwise loop will run infinitely
+- Very useful for when we don't know exactly how many times a loop should run
+"""
+
+
+
+"""
+FOR LOOPS
+
+- Often referred to as for-in loops in Python
+- Run a set number of times with specific start and end points
+- Very useful if you need to visit every element in some range or list, starting at the beginning and finishing at the end
+"""
+
