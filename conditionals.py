6cf8ffa55848eadcafcc62f7f898618fa21de9f1
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-18 20:54:50 -0400

    Add notes for conditional statements

---
+++ conditionals.py
@@ -0,0 +1,44 @@
+"""
+CONDITIONALS NOTES
+
+- Conditionals are also known as control flow
+- Allow us to choose which parts of the code to execute based on outcome of certain tests
+- Tests typically compare variables, or test single variable
+- Return true or false.
+- Can nest if statments to add subsequent tests
+- Can add multiple testing conditions in an if statement with or/and operators to test multiple conditions at once
+"""
+
+
+
+"""
+IF STATEMENTS
+
+- Execute some statements IF a test evaluates to true
+- Do nothing if test evaluates to false
+"""
+
+
+
+"""
+ELIF STATEMENTS
+
+- Short for else if
+- Allows you to chain extra if statement tests to allow for more outocmes
+- Only executed if previous test(s) fail
+- Come after if statement
+"""
+
+
+
+"""
+ELSE STATEMENTS
+
+- Provides a catch-all or failsafe
+- Only executed if previuous test(s) fail
+- Can only come after if or elif statement
+- Doesn't actually test anything, just executes code
+"""
+
+
+
