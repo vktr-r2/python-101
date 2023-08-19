877623b020ec5ac483581431ac2843f35767679f
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-18 23:33:10 -0400

    Add function notes

---
+++ functions.py
@@ -0,0 +1,29 @@
+"""
+FUNCTIONS NOTES
+
+- Contain blocks of code to be executed exactly when we choose to call the function
+- Code remains dormant until we run the function
+- Reusable => write function once, use it as many times as you need
+- Can receive inputs (paramaters) and produce outputs (return statements)
+
+
+
+PARAMETERS
+
+- Inputs into a function
+- Parameter is name for placeholder variable, argumenet is the actual value that is input
+- Functions can have 0 or more parameters
+- We use these parameters as variables within the function code
+
+
+
+RETURN VALUES
+
+- Outputs from a function
+- Not necissary to specify return type
+- Specified by a return statement within the function
+- Once return statement is reached, function stops executing and outputs the value (or no value if no return value was given)
+- Return statements are optional.  If there is no return statement in a function, function just exits after the last statement is executed
+- Can store the output as a variable when calling the function
+
+"""
